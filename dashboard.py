e8eec1351be3af763e598c6973831d83427191a9
zha2333 <zha_2333@163.com>
2026-05-05 04:44:02 +0800

    在dashboard.py顶部添加plotly强制安装

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,12 @@
+import subprocess
+import sys
+
+try:
+    import plotly
+except ModuleNotFoundError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "plotly"])
+    import plotly
+
 import streamlit as st
 import pandas as pd
 import plotly.express as px
